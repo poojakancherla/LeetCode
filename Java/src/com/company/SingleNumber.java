eed95ad552e7c9eceec2104a85ccdd5ae9cefabb
pooja kancherla <pknq6@mst.edu>
2018-01-15 18:33:59 -0600

    Finding the single number in a list by using XOR

---
+++ Java/src/com/company/SingleNumber.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class SingleNumber
+{
+    public static void main(String[] args)
+    {
+        int[] nums = new int[]{1,1,3,3,4,7,4,5,5};
+
+        int ans =0;
+
+        int len = nums.length;
+        for(int i=0;i!=len;i++)
+            ans ^= nums[i];
+
+        System.out.print(ans);
+    }
+}
