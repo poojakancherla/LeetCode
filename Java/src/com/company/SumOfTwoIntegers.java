fdd161926df9ef12ab961a0604fade0eaf6faf84
pooja kancherla <pknq6@mst.edu>
2018-01-24 15:37:44 -0600

    Adding integers without using arithmetic operators

---
+++ Java/src/com/company/SumOfTwoIntegers.java
@@ -0,0 +1,24 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class SumOfTwoIntegers
+{
+    public static void main(String[] args)
+    {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        if(a == 0) System.out.println(b);
+        if(b == 0) System.out.println(a);
+
+        while(b!= 0)
+        {
+            int carry = a & b;
+            a = a ^ b;
+            b = carry << 1;
+        }
+
+        System.out.print(a);
+    }
+}
